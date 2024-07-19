910e99d100adefdbff47e11f65702b09c5004d0d
Nikos Douvlis <nikosdouvlis@gmail.com>
2024-07-19 21:44:27 +0300

    chore(repo): Wip

--- packages/nextjs/tsup.config.ts
+++ packages/nextjs/tsup.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(overrideOptions => {
   const shouldPublish = !!overrideOptions.env?.publish;
 
   const common: Options = {
-    entry: ['./src/**/*.{ts,tsx,js,jsx}', '!./src/**/*.test.{ts,tsx}'],
+    entry: ['./src/**/*.{ts,tsx,js,jsx}', '!./src/**/*.test.{ts,tsx}', '!./src/**/server-actions.ts'],
     // We want to preserve original file structure
     // so that the "use client" directives are not lost
     // and make debugging easier via node_modules easier
@@ -37,12 +37,28 @@ export default defineConfig(overrideOptions => {
     outDir: './dist/cjs',
   };
 
+  const serverActionsEsm: Options = {
+    ...esm,
+    entry: ['./src/**/server-actions.ts'],
+    sourcemap: false,
+  };
+
+  const serverActionsCjs: Options = {
+    ...cjs,
+    entry: ['./src/**/server-actions.ts'],
+    sourcemap: false,
+  };
+
   const copyPackageJson = (format: 'esm' | 'cjs') => `cp ./package.${format}.json ./dist/${format}/package.json`;
+  const moveServerActions = (format: 'esm' | 'cjs') =>
+    `mv ./dist/${format}/server-actions.js ./dist/${format}/app-router`;
 
   return runAfterLast([
     'npm run build:declarations',
     copyPackageJson('esm'),
     copyPackageJson('cjs'),
+    moveServerActions('esm'),
+    moveServerActions('cjs'),
     shouldPublish && 'npm run publish:local',
-  ])(esm, cjs);
+  ])(esm, cjs, serverActionsEsm, serverActionsCjs);
 });
